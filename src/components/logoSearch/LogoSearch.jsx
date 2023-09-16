e8fc9d6b323ba1abb4b6a9dc99da9d85a350f7e1
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-09-16 23:39:30 +0600

    update

--- src/components/logoSearch/LogoSearch.jsx
+++ src/components/logoSearch/LogoSearch.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import Logo from '../../img/logo.png';
+import { UilSearch } from '@iconscout/react-unicons';
 function LogoSearch() {
-    return <div className="LogoSearch">LogoSearch</div>;
+    return (
+        <div className="LogoSearch">
+            <img src={Logo} alt=""></img>
+            <div className="Search">
+                <input type="text" placeholder="#Explore"></input>
+                <div className="s-icon">
+                    <UilSearch />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LogoSearch;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Home.css';
+import ProfileSide from '../../components/profileSide/ProfileSide';
 const Home = () => {
     return (
         <div className="Home">
-            <div className="profileSide">Profile</div>
+            <ProfileSide />
             <div className="postSide">Posts</div>
             <div className="rightSide">Rightside</div>
         </div>
