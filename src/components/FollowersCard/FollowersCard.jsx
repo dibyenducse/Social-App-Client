78cb064d8efaa163b9519dae0eab4adad23aef4f
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-09-18 23:02:38 +0600

    Followerscard added

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './FollowersCard.css';
-
+import { Followers } from '../../Data/FollowersData.js';
 function FollowersCard() {
     return (
-        <div className="FollowersCar">
+        <div className="FollowersCard">
             <h3>Who is following you</h3>
+            {Followers.map((follower, id) => {
+                return (
+                    <div className="follower">
+                        <div>
+                            <img
+                                src={follower.img}
+                                alt=""
+                                className="followerImage"
+                            />
+                            <div className="name">
+                                <span>{follower.name}</span>
+                                <span>{follower.usernamer}</span>
+                            </div>
+                        </div>
+                        <button className="button fc-button">Follow</button>
+                    </div>
+                );
+            })}
         </div>
     );
 }
